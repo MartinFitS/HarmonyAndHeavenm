35c4bbbf3f4fad993ce314eef56e09e7133daecb
MartinFitS <sernamartin46@gmail.com>
2023-11-01 16:19:17 -0600

    fix bugs of searcher

--- src/controllers/categoria.controller.js
+++ src/controllers/categoria.controller.js
@@ -6,18 +6,19 @@ export const categoriaProducto = async (req, res) => {
     const instrumentoTipo = req.body.instrumentoTipo;
     const marca = req.body.marca;
 
-    //where 1=1 se utiliza para consultas dinamicas
+    // where 1=1 se utiliza para consultas dinámicas
     let sql = 'SELECT * FROM products WHERE 1=1';
     const params = [];
 
-//se verifica que se haya ingresado un valor minimo. isNaN (Is Not-a-Number) verifica si un valor no es un número
-    if (!isNaN(minPrice)) {
-        sql += ' AND precioTienda >= ?';
+    if (!isNaN(minPrice) && !isNaN(maxPrice)) {
+        // Verifica si se proporcionan tanto minPrice como maxPrice
+        sql += ' AND precioPublico BETWEEN ? AND ?';
+        params.push(minPrice, maxPrice);
+    } else if (!isNaN(minPrice)) {
+        sql += ' AND precioPublico >= ?';
         params.push(minPrice);
-    }
-
-    if (!isNaN(maxPrice)) {
-        sql += ' AND precioTienda <= ?';
+    } else if (!isNaN(maxPrice)) {
+        sql += ' AND precioPublico <= ?';
         params.push(maxPrice);
     }
 
@@ -36,7 +37,52 @@ export const categoriaProducto = async (req, res) => {
             res.json(err);
         } else {
             res.render('masterPrincipalView', { products: productsf });
-            console.log(instrumentoTipo);
         }
     });
 };
+
+
+// import connection from "../bd/bdConfig";
+
+// export const categoriaProducto = async (req, res) => {
+//     const minPrice = parseFloat(req.body.minPrice);
+//     const maxPrice = parseFloat(req.body.maxPrice);
+//     const instrumentoTipo = req.body.instrumentoTipo;
+//     const marca = req.body.marca;
+
+//     console.log(minPrice, maxPrice)
+
+//     //where 1=1 se utiliza para consultas dinamicas
+//     let sql = 'SELECT * FROM products WHERE 1=1';
+//     const params = [];
+
+// //se verifica que se haya ingresado un valor minimo. isNaN (Is Not-a-Number) verifica si un valor no es un número
+//     if (!isNaN(minPrice)) {
+//         sql += ' AND precioTienda >= ?';
+//         params.push(minPrice);
+//     }
+
+//     if (!isNaN(maxPrice)) {
+//         sql += ' AND precioTienda <= ?';
+//         params.push(maxPrice);
+//     }
+
+//     if (instrumentoTipo !== 'Todos') {
+//         sql += ' AND instrumentoTipo = ?';
+//         params.push(instrumentoTipo);
+//     }
+
+//     if (marca !== 'Todos') {
+//         sql += ' AND marca = ?';
+//         params.push(marca);
+//     }
+
+//     connection.query(sql, params, (err, productsf) => {
+//         if (err) {
+//             res.json(err);
+//         } else {
+//             res.render('masterPrincipalView', { products: productsf });
+//             console.log(instrumentoTipo);
+//         }
+//     });
+// };
