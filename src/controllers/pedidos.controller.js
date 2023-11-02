0d0503bc419ee5a5afe85077482362051bc8772c
arielr <arosas7@ucol.mx>
2023-11-02 17:32:35 -0600

    Order details

--- src/controllers/pedidos.controller.js
+++ src/controllers/pedidos.controller.js
@@ -34,7 +34,7 @@ export const addPedido = async (req, res) => {
     const data = req.body;
     
     // Agregar el estado "pendiente" al pedido
-    data.estado = "pendiente";
+    data.estado = "Pendiente";
 
     await connection.query('INSERT INTO orders set ?', [data], (err, product, users) => {
       if (err) {
@@ -58,7 +58,7 @@ export const addPedido = async (req, res) => {
 
 export const editPedido = async (req, res) => {
   try {
-    const numSerie = req.body.numSerie; // Usar req.body en lugar de req.params
+    const numSerie = req.body.numSerie;
     const estado = req.body.estado;
     const usuario = req.body.idUsuario;
     const sql = 'UPDATE orders SET idUsuario = ?, estado = ? WHERE numSerie = ?';
@@ -92,10 +92,10 @@ export const pedidoAdd = async(req,res) =>{
 
 export const deletePedido = async(req,res)=>{
   const {numSerie} = req.params; // Obtener el ID del proveedor desde la URL
+  const estado = 'Cancelado'
+  const sql = 'DELETE FROM orders WHERE numSerie = ? AND estado= ?'; // Consulta SQL para eliminar el producto por su ID
 
-  const sql = 'DELETE FROM orders WHERE numSerie = ?'; // Consulta SQL para eliminar el producto por su ID
-
-  await connection.query(sql, numSerie, (err, result) => {
+  await connection.query(sql, [numSerie,estado], (err, result) => {
     if (err) {
       console.error('Error al eliminar el pedido:', err);
       res.status(500).json({ error: 'Error al eliminar el pedido' });
@@ -119,7 +119,7 @@ export const deletePedido = async(req,res)=>{
 async function obtenerPedidoPorId(numSerie){
   return new Promise((resolve,reject) =>{
     console.log(numSerie)
-    const sql = 'SELECT * FROM orders WHERE numSerie = ?';
+    const sql = ' SELECT orders.*, suppliers.*, users.id, users.username FROM orders LEFT JOIN suppliers ON orders.proveedor = suppliers.nombreProveedor LEFT JOIN users ON orders.idUsuario = users.id WHERE orders.numSerie = ?';
 
     connection.query(sql, [numSerie], (err, result)=>{
       if(err){
@@ -134,9 +134,6 @@ async function obtenerPedidoPorId(numSerie){
     })
   })
 }
-
-
-
 
 export const facturaPedido = async (req, res) => {
   const {numSerie} = req.params; 
@@ -166,21 +163,27 @@ export const facturaPedido = async (req, res) => {
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
     const formattedDate = today.toLocaleDateString('es-ES', options);
     doc.fontSize(14); // Tamaño de fuente para la fecha
-    doc.text(`Fecha: ${formattedDate}`, 30,100);
-    doc.fontSize(18); // Tamaño de fuente para el informe
-    doc.text(`Informe de Pedido ${order.numSerie}`, { align: 'center' });
-    
-    doc.text(`Número de Serie: ${order.numSerie}`, 30, 140);
-    doc.text(`Marca: ${order.marca}`, 30, 160);
-    doc.text(`Modelo: ${order.modelo}`, 30, 180);
-    doc.text(`Instrumento: ${order.instrumentoTipo}`, 30, 200);
-    doc.text(`Unidades: ${order.unidades}`, 30, 220);
-    doc.text(`Total: $${order.costoTotal}`, 30, 240);
-    doc.text(`Id del empleado: ${order.idUsuario}`, 30, 260);
-    doc.text(`Estado: ${order.estado}`, 30, 280);
-    doc.text(`Firma del empleado`, 30, 620);
-    doc.text(`Firma del proveedor`, 380, 620);
+    doc.text(`Fecha: ${formattedDate}`, 30,120, {align: 'right'});
+    doc.fontSize(16); // Tamaño de fuente para el informe
+    doc.text(`Informe del Pedido ${order.numSerie}`, 30,140,{ align: 'center' });
+    doc.text(`Pedido realizado por: ${order.username} con el ID: ${order.idUsuario}`, 30, 175);
 
-    
+    doc.text(`Número de Serie: ${order.numSerie}`, 30, 210);
+    doc.text(`Estado: ${order.estado}`, 30, 245);
+    doc.text(`Marca: ${order.marca}`, 30, 280);
+    doc.text(`Modelo: ${order.modelo}`, 30, 315);
+    doc.text(`Instrumento: ${order.instrumentoTipo}`, 30, 350);
+    doc.text(`Unidades: ${order.unidades}`, 30, 385);
+    doc.text(`Total: $${order.costoTotal} pesos`, 30, 420);
+    doc.fontSize(18);
+    doc.text(`Información sobre el proveedor:`, 30, 455);
+    doc.fontSize(16); 
+    doc.text(`Estado: ${order.nombreProveedor}`, 30, 490);
+    doc.text(`Teléfono: ${order.telefono}`, 30, 525);
+    doc.text(`Correo electrónico: ${order.correo}`, 30, 560);
+
+    doc.text(`Firma del empleado`, 30, 650, {align: 'right'});
+    doc.moveTo(380, 640).lineTo(550, 640).stroke(); // Los valores (30, 640) y (200, 640) definen los puntos de inicio y final de la línea.
+
     doc.end();
   }
